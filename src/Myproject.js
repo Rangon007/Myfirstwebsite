39a48f0a1f343f0c3f807103cbebb0735c92ba4b
Rangon <rangonsust52@gmail.com>
2023-08-24 00:34:41 +0600

    uncommenting myproject.js

--- src/Myproject.js
+++ src/Myproject.js
@@ -5,11 +5,9 @@ function Myproject() {
   return (
     <div className="myprojectcontainer">
       hello
-      {/* <ProjectItem name="rangon" dificulty="8" time="30" />
-
+      <ProjectItem name="rangon" dificulty="8" time="30" />
       <ProjectItem name="romo" dificulty="10" time="20" />
-
-      <ProjectItem name="rono" dificulty="12" time="10" /> */}
+      <ProjectItem name="rono" dificulty="12" time="10" />
     </div>
   );
 }
