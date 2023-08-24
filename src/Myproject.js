8a420c91c2eab973c347f87c4553eab97b26a033
Rangon <rangonsust52@gmail.com>
2023-08-24 23:07:39 +0600

    changing the names of project item

--- src/Myproject.js
+++ src/Myproject.js
@@ -4,9 +4,9 @@ import ProjectItem from "./ProjectItem";
 function Myproject() {
   return (
     <div className="myprojectcontainer">
-      <ProjectItem name="rijbi" dificulty="20" time="30" />
-      <ProjectItem name="karim" dificulty="10" time="20" />
-      <ProjectItem name="rono" dificulty="12" time="10" />
+      <ProjectItem name="Rangon" dificulty="20" time="30" />
+      <ProjectItem name="Romo" dificulty="10" time="20" />
+      <ProjectItem name="Rono" dificulty="12" time="10" />
     </div>
   );
 }
